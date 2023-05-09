3e114a6b72ee179a0f502f56ffbf369fe986987e
Nicolas Dubuisson <nicolas.dubuisson@etu.u-bordeaux.fr>
2023-05-09 23:27:17 +0200

    part 2 finished

--- website/src/app/home/home.component.ts
+++ website/src/app/home/home.component.ts
@@ -1,6 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, AfterViewInit, Renderer2, ElementRef } from '@angular/core';
 import { CarouselConfig } from 'ngx-bootstrap/carousel';
-import { trigger, state, style, animate, transition } from '@angular/animations';
+import { AnimationBuilder, trigger, state, style, animate, transition } from '@angular/animations';
+import { AnimationEvent } from '@angular/animations';
+
+// Ajoutez l'interface ici
+interface ProgressBar {
+  label: string;
+  value: number;
+  animationState?: string;
+}
 
 @Component({
   selector: 'app-home',
@@ -18,29 +26,70 @@ import { trigger, state, style, animate, transition } from '@angular/animations'
 
   animations: [
     trigger('animateProgressBar', [
-      state('start', style({ width: '{{ startWidth }}%' }), { params: { startWidth: 0 } }),
-      state('end', style({ width: '{{ endWidth }}%' }), { params: { endWidth: 100 } }),
-      transition('start <=> end', [
-        animate('{{ duration }}')
-      ], { params: { duration: '2s' } }),
+      state('start', style({ width: '{{startWidth}}%' }), { params: { startWidth: 0 } }),
+      state('end', style({ width: '{{endWidth}}%' }), { params: { endWidth: 10 } }),
+      transition('start => end', animate('{{duration}}')),
     ]),
   ],
+  
 
 })
 
-export class HomeComponent  {
+export class HomeComponent implements AfterViewInit {
 
-  slides: { image: string; text?: string }[] = [];
+  private observer!: IntersectionObserver;
 
-  noWrapSlides = false;
+  constructor(
+    private renderer: Renderer2,
+    private el: ElementRef,
+    private animationBuilder: AnimationBuilder
+  ) {
+    for (let i = 0; i < this.img_names.length; i++) {
+      this.addSlide(i);
+    }
+  }
 
-  progressBars = [
-    { label: 'C', value: 20 },
-    { label: 'D', value: 40 },
-    { label: 'E', value: 60 },
-    // ... autres barres de progression
+  // Mettre à jour la propriété progressBars avec le type ProgressBar[]
+  progressBars: ProgressBar[] = [
+    { label: 'C', value: 9, animationState: 'start' },
+    { label: 'C#', value: 9, animationState: 'start' },
+    { label: 'HTML-CSS-JS', value: 9, animationState: 'start' },
+    { label: 'Vue.js', value: 8, animationState: 'start' },
+    { label: 'Python', value: 8, animationState: 'start' },
+    { label: 'Java', value: 7, animationState: 'start' },
+    { label: 'Angular', value: 6, animationState: 'start' },
+    { label: 'Spring Boot', value: 6, animationState: 'start' },
+    { label: 'Ocaml', value: 6, animationState: 'start' },
+    { label: 'C++', value: 6, animationState: 'start' },
+    { label: 'React-JS', value: 4, animationState: 'start' },
+    { label: 'React-Native', value: 3, animationState: 'start' },
+    { label: 'PHP', value: 2, animationState: 'start' },
+    { label: 'SQL', value: 2, animationState: 'start' },
+
+    // Ajoutez d'autres objets de progression ici
   ];
 
+  ngAfterViewInit(): void {
+    this.observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          const progressBar = this.progressBars.find((bar) => bar.label === entry.target.id);
+          if (progressBar) {
+            if (entry.isIntersecting) {
+              progressBar.animationState = 'end';
+            } else {
+              progressBar.animationState = 'start';
+            }
+          }
+        });
+      },
+      { threshold: 0.5 }
+    );
+  
+    const progressBars = this.el.nativeElement.querySelectorAll('.progress-bar');
+    progressBars.forEach((bar:any) => this.observer.observe(bar));
+  }
+  
   onAnimationEvent(event: AnimationEvent, progressBar: any) {
     if (event.fromState === 'end') {
       progressBar.animationState = 'start';
@@ -48,6 +97,13 @@ export class HomeComponent  {
       progressBar.animationState = 'end';
     }
   }
+
+  // --------------------------------------------------------
+  
+  slides: { image: string; text?: string }[] = [];
+
+  noWrapSlides = false;
+
   
   img_names = [
     { name: "android.png", text : "Mon jeu vidéo"},
@@ -57,11 +113,6 @@ export class HomeComponent  {
     { name: "image_pro.png", text : "Projet Universitaire"},
   ];
 
-  constructor() {
-    for (let i = 0; i < this.img_names.length; i++) {
-      this.addSlide(i);
-    }
-  }
 
   addSlide(i: number): void {
     this.slides.push({
